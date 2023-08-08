5361762aff78a3a461b4d32e481737a32a7111b8
hlx <1198816371@qq.com>
2023-08-08 15:10:27 +0000

    ch3

--- tests/hello.sh
+++ tests/hello.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#! /bin/bash
 
 test_name=$(basename "$0" .sh)
 t=out/tests/$test_name
 
 mkdir -p "$t"
 
-cat <<EOF | riscv64-linux-gnu-gcc -o "$t"/a.o -c -xc -
+cat <<EOF | $CC -o "$t"/a.o -c -xc -
 #include <stdio.h>
 
 int main(){
@@ -14,4 +14,4 @@ int main(){
 }
 EOF
 
-./rvld "$t"/a.o
+$CC -B. -static "$t"/a.o -o "$t"/out
